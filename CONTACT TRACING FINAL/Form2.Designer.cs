d1525e9233238ed2794f4f9e4bf05a0803dde0f2
unknown <migzberneri@gmail.com>
2022-07-11 11:43:22 +0800

    added form 2 for filtering, searching records

--- CONTACT TRACING FINAL/Form2.Designer.cs
+++ CONTACT TRACING FINAL/Form2.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.showtxbx = new System.Windows.Forms.TextBox();
+            this.Showallbtn = new System.Windows.Forms.Button();
+            this.searchbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // showtxbx
+            // 
+            this.showtxbx.Location = new System.Drawing.Point(65, 43);
+            this.showtxbx.Multiline = true;
+            this.showtxbx.Name = "showtxbx";
+            this.showtxbx.Size = new System.Drawing.Size(663, 234);
+            this.showtxbx.TabIndex = 0;
+            // 
+            // Showallbtn
+            // 
+            this.Showallbtn.Location = new System.Drawing.Point(95, 391);
+            this.Showallbtn.Name = "Showallbtn";
+            this.Showallbtn.Size = new System.Drawing.Size(108, 26);
+            this.Showallbtn.TabIndex = 1;
+            this.Showallbtn.Text = "Show All";
+            this.Showallbtn.UseVisualStyleBackColor = true;
+            // 
+            // searchbtn
+            // 
+            this.searchbtn.Location = new System.Drawing.Point(522, 391);
+            this.searchbtn.Name = "searchbtn";
+            this.searchbtn.Size = new System.Drawing.Size(75, 27);
+            this.searchbtn.TabIndex = 2;
+            this.searchbtn.Text = "Search";
+            this.searchbtn.UseVisualStyleBackColor = true;
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(653, 391);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(75, 27);
+            this.deletebtn.TabIndex = 3;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(262, 391);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.searchbtn);
+            this.Controls.Add(this.Showallbtn);
+            this.Controls.Add(this.showtxbx);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox showtxbx;
+        private System.Windows.Forms.Button Showallbtn;
+        private System.Windows.Forms.Button searchbtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
