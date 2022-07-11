e0870d720d60df269a57405636599c16938785ed
unknown <migzberneri@gmail.com>
2022-07-11 10:46:58 +0800

    added buttons, labels and textboxes already renamed

---
+++ CONTACT TRACING FINAL/Form1.Designer.cs
@@ -0,0 +1,429 @@
+﻿namespace CONTACT_TRACING_FINAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.namelbl = new System.Windows.Forms.Label();
+            this.agelbl = new System.Windows.Forms.Label();
+            this.bdaylbl = new System.Windows.Forms.Label();
+            this.genderlbl = new System.Windows.Forms.Label();
+            this.contctlbl = new System.Windows.Forms.Label();
+            this.Emaillbl = new System.Windows.Forms.Label();
+            this.addresslbl = new System.Windows.Forms.Label();
+            this.q1 = new System.Windows.Forms.Label();
+            this.q2 = new System.Windows.Forms.Label();
+            this.q3 = new System.Windows.Forms.Label();
+            this.q4 = new System.Windows.Forms.Label();
+            this.usernamelbl = new System.Windows.Forms.Label();
+            this.passlbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.Btnadmin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btnsubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-2, -3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(941, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Personal Information";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-2, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(941, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Contact Tracing Questions";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(-2, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(941, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Personnel Only";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // namelbl
+            // 
+            this.namelbl.AutoSize = true;
+            this.namelbl.Location = new System.Drawing.Point(33, 41);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(55, 20);
+            this.namelbl.TabIndex = 3;
+            this.namelbl.Text = "Name:";
+            // 
+            // agelbl
+            // 
+            this.agelbl.AutoSize = true;
+            this.agelbl.Location = new System.Drawing.Point(33, 100);
+            this.agelbl.Name = "agelbl";
+            this.agelbl.Size = new System.Drawing.Size(42, 20);
+            this.agelbl.TabIndex = 4;
+            this.agelbl.Text = "Age:";
+            // 
+            // bdaylbl
+            // 
+            this.bdaylbl.AutoSize = true;
+            this.bdaylbl.Location = new System.Drawing.Point(33, 149);
+            this.bdaylbl.Name = "bdaylbl";
+            this.bdaylbl.Size = new System.Drawing.Size(71, 20);
+            this.bdaylbl.TabIndex = 5;
+            this.bdaylbl.Text = "Birthday:";
+            // 
+            // genderlbl
+            // 
+            this.genderlbl.AutoSize = true;
+            this.genderlbl.Location = new System.Drawing.Point(449, 41);
+            this.genderlbl.Name = "genderlbl";
+            this.genderlbl.Size = new System.Drawing.Size(67, 20);
+            this.genderlbl.TabIndex = 6;
+            this.genderlbl.Text = "Gender:";
+            // 
+            // contctlbl
+            // 
+            this.contctlbl.AutoSize = true;
+            this.contctlbl.Location = new System.Drawing.Point(449, 83);
+            this.contctlbl.Name = "contctlbl";
+            this.contctlbl.Size = new System.Drawing.Size(82, 20);
+            this.contctlbl.TabIndex = 7;
+            this.contctlbl.Text = "Contact #:";
+            // 
+            // Emaillbl
+            // 
+            this.Emaillbl.AutoSize = true;
+            this.Emaillbl.Location = new System.Drawing.Point(449, 135);
+            this.Emaillbl.Name = "Emaillbl";
+            this.Emaillbl.Size = new System.Drawing.Size(52, 20);
+            this.Emaillbl.TabIndex = 8;
+            this.Emaillbl.Text = "Email:";
+            // 
+            // addresslbl
+            // 
+            this.addresslbl.AutoSize = true;
+            this.addresslbl.Location = new System.Drawing.Point(696, 41);
+            this.addresslbl.Name = "addresslbl";
+            this.addresslbl.Size = new System.Drawing.Size(72, 20);
+            this.addresslbl.TabIndex = 9;
+            this.addresslbl.Text = "Address:";
+            // 
+            // q1
+            // 
+            this.q1.AutoSize = true;
+            this.q1.Location = new System.Drawing.Point(43, 242);
+            this.q1.Name = "q1";
+            this.q1.Size = new System.Drawing.Size(453, 20);
+            this.q1.TabIndex = 10;
+            this.q1.Text = "Have you experiencing or experienced the following symptoms?";
+            // 
+            // q2
+            // 
+            this.q2.AutoSize = true;
+            this.q2.Location = new System.Drawing.Point(43, 296);
+            this.q2.Name = "q2";
+            this.q2.Size = new System.Drawing.Size(387, 20);
+            this.q2.TabIndex = 11;
+            this.q2.Text = "If yes, specify the date when these symptoms started:";
+            // 
+            // q3
+            // 
+            this.q3.AutoSize = true;
+            this.q3.Location = new System.Drawing.Point(48, 335);
+            this.q3.Name = "q3";
+            this.q3.Size = new System.Drawing.Size(397, 20);
+            this.q3.TabIndex = 12;
+            this.q3.Text = "Have you been with someone who has been exposed?:";
+            // 
+            // q4
+            // 
+            this.q4.AutoSize = true;
+            this.q4.Location = new System.Drawing.Point(48, 371);
+            this.q4.Name = "q4";
+            this.q4.Size = new System.Drawing.Size(188, 20);
+            this.q4.TabIndex = 13;
+            this.q4.Text = "Are you fully-vaccinated?:";
+            // 
+            // usernamelbl
+            // 
+            this.usernamelbl.AutoSize = true;
+            this.usernamelbl.Location = new System.Drawing.Point(307, 453);
+            this.usernamelbl.Name = "usernamelbl";
+            this.usernamelbl.Size = new System.Drawing.Size(87, 20);
+            this.usernamelbl.TabIndex = 14;
+            this.usernamelbl.Text = "Username:";
+            // 
+            // passlbl
+            // 
+            this.passlbl.AutoSize = true;
+            this.passlbl.Location = new System.Drawing.Point(307, 485);
+            this.passlbl.Name = "passlbl";
+            this.passlbl.Size = new System.Drawing.Size(82, 20);
+            this.passlbl.TabIndex = 15;
+            this.passlbl.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(216, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(331, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(101, 94);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(101, 149);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 20;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(528, 41);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(528, 83);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 26);
+            this.textBox7.TabIndex = 22;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(528, 132);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 26);
+            this.textBox8.TabIndex = 23;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(700, 64);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(201, 97);
+            this.textBox9.TabIndex = 24;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(518, 239);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 26);
+            this.textBox10.TabIndex = 25;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(436, 293);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 26);
+            this.textBox11.TabIndex = 26;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(451, 335);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 26);
+            this.textBox12.TabIndex = 27;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(242, 371);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 26);
+            this.textBox13.TabIndex = 28;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(400, 453);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 26);
+            this.textBox14.TabIndex = 29;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(400, 485);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(100, 26);
+            this.textBox15.TabIndex = 30;
+            // 
+            // Btnadmin
+            // 
+            this.Btnadmin.Location = new System.Drawing.Point(543, 485);
+            this.Btnadmin.Name = "Btnadmin";
+            this.Btnadmin.Size = new System.Drawing.Size(75, 29);
+            this.Btnadmin.TabIndex = 31;
+            this.Btnadmin.Text = "log in";
+            this.Btnadmin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 20);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "(Loss of taste, Loss of smell, Shortness of breath, cough, fever) ";
+            // 
+            // Btnsubmit
+            // 
+            this.Btnsubmit.Location = new System.Drawing.Point(809, 368);
+            this.Btnsubmit.Name = "Btnsubmit";
+            this.Btnsubmit.Size = new System.Drawing.Size(75, 29);
+            this.Btnsubmit.TabIndex = 33;
+            this.Btnsubmit.Text = "Submit";
+            this.Btnsubmit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 526);
+            this.Controls.Add(this.Btnsubmit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btnadmin);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passlbl);
+            this.Controls.Add(this.usernamelbl);
+            this.Controls.Add(this.q4);
+            this.Controls.Add(this.q3);
+            this.Controls.Add(this.q2);
+            this.Controls.Add(this.q1);
+            this.Controls.Add(this.addresslbl);
+            this.Controls.Add(this.Emaillbl);
+            this.Controls.Add(this.contctlbl);
+            this.Controls.Add(this.genderlbl);
+            this.Controls.Add(this.bdaylbl);
+            this.Controls.Add(this.agelbl);
+            this.Controls.Add(this.namelbl);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CONTACT TRACING";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label namelbl;
+        private System.Windows.Forms.Label agelbl;
+        private System.Windows.Forms.Label bdaylbl;
+        private System.Windows.Forms.Label genderlbl;
+        private System.Windows.Forms.Label contctlbl;
+        private System.Windows.Forms.Label Emaillbl;
+        private System.Windows.Forms.Label addresslbl;
+        private System.Windows.Forms.Label q1;
+        private System.Windows.Forms.Label q2;
+        private System.Windows.Forms.Label q3;
+        private System.Windows.Forms.Label q4;
+        private System.Windows.Forms.Label usernamelbl;
+        private System.Windows.Forms.Label passlbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Button Btnadmin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btnsubmit;
+    }
+}
+
